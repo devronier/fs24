b0474b0f12898aed8b993fffb2e958c710d769da
Ronier Lima <ronier.lim@gmail.com>
2024-05-04 16:53:09 -0300

    update

--- aulas/2024-05-04/src/app.js
+++ aulas/2024-05-04/src/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes");
+const authMiddleware = require("./middlewares/authMiddleware");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/users", userRoutes);
+app.get("/", authMiddleware, (request, response) => {
+  response.send("Hello word");
+});
+
+app.use("/auth", authRoutes);
+
+app.use("/users", authMiddleware, userRoutes);
 
 app.listen(8080, () => {
   console.log("estou rodando");
@@ -22,3 +30,14 @@ app.listen(8080, () => {
 
 // Gerar de alguma forma, id unicos para cada usuario ao cadastrar
 //ex: Vc pode usar sequenciais ou outra forma,
+
+// Separa as responsabilidades de auth (criar controller, services e repository se precisar)
+
+// Definir um tempo de 30 minutos de expiracao para cada token
+
+// Fazer a busca de usuario pelo email no login ser feita pelo usuariosRepository
+
+// criar um crud de tarefas e cada tarefa pertence a um usuario
+
+// autorizacao, apenas um tipo, como ADMIN vai poder mexer com usuarios e
+// usuarios comuns vao poder mexer com tarefas
